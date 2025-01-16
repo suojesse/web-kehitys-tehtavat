31f62eebd4d526248c825cb2e2b513a505a4550e
suojesse <suojesse@gmail.com>
2025-01-16 14:45:10 +0200

    muutoksia index.js fileen

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-const username = 'John Doe';
-console.log('WAZZUUUUUUPPP, ' + username + '!');
+// index.js
+import http from 'http';
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Welcome to my REST API!');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
